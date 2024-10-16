6c51f468d53cea2ad9bf8c75fa33a7f1132b7ddc
Igor <igor.arnautovic123@outlook.com>
2024-10-17 01:14:05 +0200

    Created the User model

--- mysite/login/models.py
+++ mysite/login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
